8b94130b927ad871fb2d14363edabb1ec0a4e612
bfrazier <bthehippie2@gmail.com>
2023-01-14 16:22:57 -0500

    save new note to db.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs')
 const db = require('./db/db.json');
 
 const app = express();
@@ -17,6 +18,44 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
+
+// post new note to database file
+app.post('/notes', (req, res) => {
+    
+    // items that will be in the body of the object
+    const { title, text } = req.body;
+
+    if(title && text){
+        // variable to save the object
+        const newNote = {
+            title,
+            text
+        };
+
+        // read the file, add new note object
+        const oldNotes = fs.readFileSync(`./db/db.json`);
+        const notes = JSON.parse(oldNotes);
+        notes.push(newNote)
+
+        // convert the data to a string so it can be saved
+        const noteString = JSON.stringify(notes, null, 2);
+
+        // write the string to the database file
+        fs.writeFile(`./db/db.json`, noteString, (err) => 
+            err ? console.error(err) : console.log(`New note ${newNote.title} added to file.`)
+        );
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting review');
+    }
+});
 
 app.listen(PORT, () =>
     console.log(`View at http://localhost:${PORT}`)
