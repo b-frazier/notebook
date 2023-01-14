2545040329c0b42c8d22412d10a3e1d08079217b
bfrazier <bthehippie2@gmail.com>
2023-01-14 15:37:37 -0500

    add middleware

--- Develop/server.js
+++ Develop/server.js
@@ -5,6 +5,9 @@ const db = require('./db/db.json');
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => 
