509208fb8db6fa33d5934722cf08f66fa4b0ab6a
bfrazier <bthehippie2@gmail.com>
2023-01-15 15:21:47 -0500

    move code to api route file

--- Develop/server.js
+++ Develop/server.js
@@ -24,45 +24,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
 });
 
-// post new note to database file
-app.post('/api/notes', (req, res) => {
-    
-    // items that will be in the body of the object
-    const { title, text } = req.body;
-
-    if(title && text){
-        // variable to save the object
-        const newNote = {
-            title,
-            text,
-            id: uniqid()
-        };
-
-        // read the file, add new note object
-        const oldNotes = fs.readFileSync(`./db/db.json`);
-        const notes = JSON.parse(oldNotes);
-        notes.push(newNote)
-
-        // convert the data to a string so it can be saved
-        const noteString = JSON.stringify(notes, null, 2);
-
-        // write the string to the database file
-        fs.writeFile(`./db/db.json`, noteString, (err) => 
-            err ? console.error(err) : console.log(`New note ${newNote.title} added to file.`)
-        );
-
-        const response = {
-            status: 'success',
-            body: newNote,
-        };
-
-        console.log(response);
-        res.status(201).json(response);
-    } else {
-        res.status(500).json('Error in posting review');
-    }
-});
-
 app.listen(PORT, () =>
     console.log(`View at http://localhost:${PORT}`)
 );
