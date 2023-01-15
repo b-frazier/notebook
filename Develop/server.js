b3cce0619acf0af4a85dc6e541c0c3ec0df75561
bfrazier <bthehippie2@gmail.com>
2023-01-15 15:27:12 -0500

    remove unused code, require routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,4 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs')
-const uniqid = require('uniqid');
-const { dirname } = require('path');
 
 const app = express();
 const PORT = 3001;
@@ -12,11 +8,8 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-);
-
-
+require('./routes/apiRoute')(app);
+require('./routes/htmlRoute')(app);
 
 app.listen(PORT, () =>
     console.log(`View at http://localhost:${PORT}`)
